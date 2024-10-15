8364a02a1a5785b5667b8c763d5280a35b0f5111
ds-wook <leewook94@gmail.com>
2024-10-15 18:50:23 +0900

    feat: upload code!

---
+++ experiment/data/dataset.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Tuple
+
+import pandas as pd
+from omegaconf import DictConfig
+from sklearn.model_selection import StratifiedKFold
+
+
+def load_train_dataset(cfg: DictConfig) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train = pd.read_csv(Path(cfg.data.path) / "train.csv")
+    kfold = StratifiedKFold(n_splits=cfg.data.n_splits, shuffle=True, random_state=cfg.data.seed)
+    train_idx, valid_idx = next(kfold.split(train))
+
+    X_train = train[cfg.store.selected_features].iloc[train_idx]
+    y_train = train[cfg.data.target].iloc[train_idx]
+    X_valid = train[cfg.store.selected_features].iloc[valid_idx]
+    y_valid = train[cfg.data.target].iloc[valid_idx]
+
+    return X_train, y_train, X_valid, y_valid
+
+
+def load_test_dataset(cfg: DictConfig) -> pd.DataFrame:
+    test = pd.read_csv(Path(cfg.data.path) / "test.csv")
+    X_test = test[cfg.store.selected_features]
+    return X_test
--- notebook/eda.py
+++ notebook/eda.py
@@ -1,3 +1,10 @@
-# %%
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+
+train = pd.read_csv("input/loan-approval-prediction/train.csv")
+
+train.info()
+# %%
+train.head()
